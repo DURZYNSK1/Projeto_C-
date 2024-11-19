aa835e3816bb19261f44e56ea05d348cd5c2571c
Gabriel Durzynski <durzynski.dev@gmail.com>
2024-11-19 10:34:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PROJETO_02.Models;
+
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa();
+Pessoa p2 = new Pessoa();
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 3, valorDiaria: 30);
+
+
+Reserva reserva = new Reserva(diasReservados: 10);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+Console.WriteLine($"Hospedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
